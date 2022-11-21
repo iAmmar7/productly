c2d3e7ad72a5b6b51ff4eb73665f4510b84a0f3e
Ammar Ansari <iammaransari@gmail.com>
2022-11-22 01:11:15 +0500

    pagination section

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,9 +1,23 @@
+import { Fragment } from 'react';
+
+import { Pagination } from '../Pagination';
 import Listing from './Listing';
 
 function Products(props) {
   const { products } = props;
 
-  return <Listing products={products} />;
+  return (
+    <Fragment>
+      <section>
+        <h2 className='sr-only'>Product listing</h2>
+        <Listing products={products} />
+      </section>
+      <section>
+        <h2 className='sr-only'>Pagination</h2>
+        <Pagination />
+      </section>
+    </Fragment>
+  );
 }
 
 export default Products;
